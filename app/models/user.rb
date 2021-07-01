bd0880f66cfd569033c9e8d4ad8ef8d641277372
hadzuki89 <k_hadzuki89@icloud.com>
2021-07-01 15:54:10 +0900

    新規登録機能の実装＆ログイン、ログアウト機能の実装
    新規登録機能実装時のコミットを忘れていました

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :email, presence: true, uniqueness: true
+  validates :name, :password, :last_name, :first_name, :last_name_kana, :first_name_kana, :birth_day, presence: true
+  has_many :items
+  has_many :purchase_records
+  
 end
