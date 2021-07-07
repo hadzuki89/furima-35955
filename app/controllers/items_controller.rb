8353f22dd39f00d54a5fb5234927822920c00132
hadzuki89 <k_hadzuki89@icloud.com>
2021-07-07 16:27:03 +0900

    修正３

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update]
   before_action :contributor_confirmation, only: [:edit, :update]
 
   def index
@@ -20,16 +21,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
-
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path
     else
@@ -44,8 +41,11 @@ class ItemsController < ApplicationController
                                  :price).merge(user_id: current_user.id)
   end
 
-  def contributor_confirmation
+  def set_item
     @item = Item.find(params[:id])
+  end
+
+  def contributor_confirmation
     redirect_to root_path unless current_user == @item.user
   end
 end
